312e46cc5f80434f03aa79597d756f9c3943a5ec
EDILSON LUCIANO DOS SANTOS <140564587+Edilson591@users.noreply.github.com>
2024-02-20 16:02:00 -0300

    fix code

--- index.js
+++ index.js
@@ -1,24 +1,35 @@
-let name = "machine"
-let i = 10000
-const nivel = {fe:"ferro",br:"Bronze",pr:"Prata", ou:"Ouro", pl:"Platina", as:"Ascendente", im:"Imortal", ra:"Radiante"}
 
-    if(i <= 1000) {
-        console.log(`O Héroi de ${name} está no nivel ${nivel.fe}`)
-    }else if(i >= 1001 && i <= 2000){
-        console.log(`O Héroi de ${name} está no nivel ${nivel.br}`)
-    }else if(i >= 2001 && i <= 5000){
-        console.log(`O Héroi de ${name} está no nivel ${nivel.pr}`)
-    }else if(i >= 6001 && i <= 7000){
-        console.log(`O Héroi de ${name} está no nivel ${nivel.ou}`)
-    }else if(i >= 7001 && i <= 8000){
-        console.log(`O Héroi de ${name} está no nivel ${nivel.pl}`)
-    }else if(i >= 8001 && i <= 9000){
-        console.log(`O Héroi de ${name} está no nivel ${nivel.as}`)
-    }else if(i >= 9001 && i <= 10000){
-        console.log(`O Héroi de ${name} está no nivel ${nivel.im}`)
-    }else{
-        console.log(`O Héroi de ${name} está no nivel ${nivel.ra}`)
-    } 
+
+const hero = {
+    name:"Machine",
+    level:10000
+}
+
+const levels = [
+    {level: [0, 1000 ], name:"Ferro"},
+    {level: [1001, 2000 ], name:"Bronze"},
+    {level: [2001, 5000 ], name:"Platina"},
+    {level: [6001, 7000 ], name:"Ouro"},
+    {level: [7001, 8000 ], name:"Platina"},
+    {level: [8001, 9000 ], name:"Ascendente"},
+    {level: [9001, 10000 ], name:"Imortal"}
+]
+
+
+function getLevels(hero, levels) {
+    for(let level of levels) {
+        if(hero.level >= level.level[0] && hero.level <= level.level[1]){
+            return level.name
+        }
+    }
+    
+    return "Radiante"
+}
+
+const getTools = getLevels(hero,levels)
+
+console.log(`O Héroi de ${hero.name} está no nivel ${heroLevel}`)
+
 
 
 
